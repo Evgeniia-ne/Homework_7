f3393c6c2a8c359f62019c4518dc2552465dc889
Asus <koncarik@mail.ru>
2022-12-22 00:33:18 +0500

    Найдите среднее арифметическое элементов в каждом столбце

---
+++ Zadacha_52/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+void ShowArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+int DataEntry(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+//Получить от пользователя числа m и n
+int m = DataEntry("Введите число m: ");
+int n = DataEntry("Введите число n: ");
+
+//Создать двухмерный массив и заполнить
+int[,] matrix = CreateRandomArray(m, n, 0, 10);
+
+//Вывести на экран
+ShowArray(matrix);
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        avarage = (avarage + matrix[i, j]);
+    }
+    avarage = Math.Round(avarage / n, 1);
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
